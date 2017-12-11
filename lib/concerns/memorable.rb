8d39877e8c65391900897a604fe055db4bccd459
Alex McKean <amckean@me.com>
2017-12-11 00:58:56 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,16 @@
 module Memorable
-  def reset_all
-    self.all.clear
-  end
+  module ClassMethods
 
-  def count
-    self.all.count
-  end 
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+
+  module InstanceMethods
+    def initialize
+    end 
+
 end
